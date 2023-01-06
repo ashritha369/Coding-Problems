f83c7ce1ecd5c05010e5a5de29feeb6ab37f4aa2
ashritha369 <ashrithagowda666@gmail.com>
2023-01-06 14:12:00 +0530

    DNA TO RNA CONVERSION SOLUTION

--- dnaToRna.js
+++ dnaToRna.js
@@ -1,18 +1,20 @@
 // https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
 // replace()
 
-// QUESTON
-// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+// QUESTION
+/*Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
-// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
 
-// Create a function which translates a given DNA string into RNA.
+ Create a function which translates a given DNA string into RNA.
 
-// For example:
+For example:
 
-// "GCAT"  =>  "GCAU"
-// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
-// MYSOLUTION
+ "GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+
+// MY SOLUTION
 function DNAtoRNA(dna) {
   // create a function which returns an RNA sequence from the given DNA sequence
   console.log(dna.replace(/T/g, "U"));
