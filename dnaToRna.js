3e56a661e053794e9b5d9c9d879eee6af8a3d2ac
ashritha369 <ashrithagowda666@gmail.com>
2023-01-06 14:04:38 +0530

    DNA TO RNA CONVERSION SOLUTION

--- dnaToRna.js
+++ dnaToRna.js
@@ -1,1 +1,23 @@
 // https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+// replace()
+
+// QUESTON
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+// MYSOLUTION
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  console.log(dna.replace(/T/g, "U"));
+  return dna.replace(/T/g, "U");
+}
+DNAtoRNA("TTTT");
+DNAtoRNA("GCAT");
+DNAtoRNA("GACCGCCGCC");
---
+++ documentation.md
@@ -0,0 +1,11 @@
+Pushing codewars solution to github
+[https://youtu.be/aoBOUmO4QP0](https://youtu.be/aoBOUmO4QP0)
+
+---
+
+- includes()-->[https://www.w3schools.com/jsref/jsref_includes.asp](https://www.w3schools.com/jsref/jsref_includes.asp)
+- indexOf()-->[https://www.w3schools.com/jsref/jsref_indexof.asp](https://www.w3schools.com/jsref/jsref_indexof.asp)
+- replace()-->[https://www.w3schools.com/jsref/jsref_replace.asp](https://www.w3schools.com/jsref/jsref_replace.asp)
+- In a function, always remember to return the values.
+
+---
