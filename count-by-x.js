e8460ad8c1df0a84b7e223f17959df80fa035866
ashritha369 <ashrithagowda666@gmail.com>
2023-04-18 04:38:00 +0530

    count by x

---
+++ count-by-x.js
@@ -0,0 +1,28 @@
+// Count by Xs : https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+// Same problem but  name as 'Count Monkeys' with scenario: https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+//-------------------------------------------------------------------------------------------
+
+// WE WILL SOLVE COUNT BY Xs : https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+/*
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+Examples
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+*/
+//MY SOLUTION:
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+
+  return z;
+}
