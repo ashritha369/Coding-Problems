e364f6bde6ae89038bff7ede8798be869ff54b61
ashritha369 <ashrithagowda666@gmail.com>
2023-04-19 04:59:46 +0530

    hello + name

---
+++ grasshopper-debug-say-hello.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/javascript
+/*Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+FUNDAMENTALS
+*/
+
+// My Attempt
+function sayHello(name) {
+  return "Hello" + "," + " " + name;
+}
