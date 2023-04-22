63a048e475a3ee8a25353eda6ef7f33a3d3e9455
ashritha369 <ashrithagowda666@gmail.com>
2023-04-22 22:11:05 +0530

    added string-repeat

---
+++ string-repeat.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+/*Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+FUNDAMENTALSSTRINGS
+*/
+function repeatStr(n, s) {
+  let concatedString = "";
+  for (let i = 0; i < n; i++) {
+    concatedString = concatedString + s;
+  }
+  return concatedString;
+}
