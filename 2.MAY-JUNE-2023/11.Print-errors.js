d0d8c9b58001dec4a10940bf4f9962d863526f29
ashritha369 <ashrithagowda666@gmail.com>
2023-06-01 04:47:48 +0530

    other solution for print errors

--- 2.MAY-JUNE-2023/11.Print-errors.js
+++ 2.MAY-JUNE-2023/11.Print-errors.js
@@ -25,3 +25,16 @@ function printerError(s) {
   return `${numerator}/${denominator}`;
 }
 // the regular expression /[n-z]/g is used to match the letters "n" to "z" in the string 's'
+
+//OR OTHER SOLUTION
+
+function printerError(s) {
+  // your code
+  var count = 0;
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] > "m") {
+      count++;
+    }
+  }
+  return count + "/" + s.length;
+}
