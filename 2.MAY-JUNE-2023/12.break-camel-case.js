1faf4aed640b4b8df259496d5950703d3cfb454c
ashritha369 <ashrithagowda666@gmail.com>
2023-06-06 04:40:11 +0530

    break-camel-case

---
+++ 2.MAY-JUNE-2023/12.break-camel-case.js
@@ -0,0 +1,35 @@
+//https://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+*/
+function solution(string) {
+  let array = [];
+
+  while (string.length !== 0) {
+    let index = 0;
+    for (let i = 1; i < string.length; i++) {
+      if (string[i] === string[i].toUpperCase()) {
+        index = i;
+        break;
+      }
+    }
+    if (index === 0) {
+      index = string.length;
+    }
+    let beginString = string.slice(0, index);
+    console.log(`Begin String, ${beginString}`);
+    array.push(beginString);
+    string = string.slice(index);
+    console.log(`string ${string}`);
+  }
+
+  return array.join(" ");
+}
+
+solution("camelCasingTest");
