3bd89514e4e1a6c6bd4b007591b26c4357676bec
ashritha369 <ashrithagowda666@gmail.com>
2023-06-12 11:20:14 +0530

    working on logic

---
+++ 2.MAY-JUNE-2023/13.count-characters-in-your-string.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
+
+/*
+const myObject={
+    key1:value1,
+    key2:value2,
+    key3:value3
+}
+*/
+
+/*
+ const keys:[] ;
+ const values=[];
+*/
