940096b3c55fcdf99e9e13c3da8f09dd8c2564a4
unknown <ashrithagowda666@gmail.com>
2023-12-15 14:40:28 +0530

    number-of-good-pairs

---
+++ 3)Data-Structure-and-Algorithms-Coding/Arrays/4.Number-of-good-pairs.js
@@ -0,0 +1,16 @@
+
+//https://leetcode.com/problems/number-of-good-pairs/
+
+var numIdenticalPairs = function(nums) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+                  if(nums[i]===nums[j]){
+                      count++;
+                  }
+        }
+
+    }
+    return count;
+};
+numIdenticalPairs(1,2,3,1,1,3)
