f61cb0e65bcf9673f64238e03f1b1c1ce787f139
unknown <ashrithagowda666@gmail.com>
2024-01-05 07:56:33 +0530

    DSC Array: left and right sum:better time ans space complexity

--- 3)Data-Structure-and-Algorithms-Coding/2)Data-Structures/1.Arrays/17.Left-and-Right-Sum-Difference.js
+++ 3)Data-Structure-and-Algorithms-Coding/2)Data-Structures/1.Arrays/17.Left-and-Right-Sum-Difference.js
@@ -4,11 +4,10 @@ var leftRightDifference = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     let leftSum = 0;
     let rightSum = 0;
-    let leftArray = [0];
-    let rightArray = [0];
 
-    leftArray = nums.slice(0, i);
-    rightArray = nums.slice(i + 1, nums.length + 1);
+    // Slicing leftArray and rightArray correctly
+    let leftArray = nums.slice(0, i);
+    let rightArray = nums.slice(i + 1);
 
     leftSum = leftArray.reduce((acc, curr) => acc + curr, 0);
     rightSum = rightArray.reduce((acc, curr) => acc + curr, 0);
@@ -17,3 +16,19 @@ var leftRightDifference = function (nums) {
   }
   return answer;
 };
+
+console.log(leftRightDifference([6, 4, 1, 2, 9]));
+
+//  BETTER TIME AND SPACE COMPLEXITIES
+var leftRightDifference = function (nums) {
+  let totalSum = nums.reduce((acc, curr) => acc + curr, 0);
+  let leftSum = 0;
+  let answer = [];
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = Math.abs(leftSum - (totalSum - leftSum - nums[i]));
+    leftSum += nums[i];
+  }
+  return answer;
+};
+
+console.log(leftRightDifference([6, 4, 1, 2, 9]));
